b4afa444c82770fb37451912e5c58e4c19a3610b
RenanFlorencio <renanm.florencio@gmail.com>
2024-10-03 16:38:04 -0300

    recebendo rotinas com o LLAMA e trasformando em trips

--- completo/LLAMAconnect.py
+++ completo/LLAMAconnect.py
@@ -41,39 +41,32 @@ def getResponse_xml(path, veh_id, departure_time, parking_area_duration):
 
 def getResponse_trip(student_info):
 
-    instructions_content = (f"Plan the information of the following student: {student_info}. " + 
-                            "Here is the example of the routine of an integral Computer Engineering student: " +
-                            "{'time':'7:00'  'location':'home' 'activity':'preparing for classes' />" +
-                            "{'time':'8:00'  'location':'FEEC' 'activity':'class' }, " +
-                            "{'time':'12:00' 'location':'RA'   'activity':'lunch' }, " +
-                            "{'time':'14:00' 'location':'IC'   'activity':'class' }, " +
-                            "{'time':'17:00' 'location':'IC'   'activity':'study' }, " +
-                            "{'time':'18:00' 'location':'RS'   'activity':'dinner'}, " +
-                            "{'time':'20:00' 'location':'home' 'activity':'relax' }, "  
+    instructions_content = (f"Plan the information of the following student: {student_info}. Students do not have classes during lunch and dinner time" 
                             )
     message = [
         {
             "role": "system",
             "content": "You have to plan the routine of a student that is going to be between important places inside UNICAMP, you can move between any of " + 
-                            "the following places: RU, RA, RS, BC, FEQ, IC, IFGW, IMECC, FEF, FCM, IB, IQ, IB, FCM, FCM, IMECC, IFGW, IC, FEEC. " +
+                            "the following places: HOME, RU, RA, RS, BC, FEQ, IC, IFGW, IMECC, FEF, FCM, IB, IQ, IB, FCM, FCM, IMECC, IFGW, IC, FEEC. " +
                             "The trip must make sense on the given context. For example, a Chemistry student would spend most of their time " +
                             "at IQ, while a Computer Science student would spend most of their time at IC. " +
-                            "Note that RU, RA and RS are restaurants where students go to have breakfast, luch and dinner. " +
-                            "Every student must have lunch and dinner at some of the three restaurants. " + 
-                            "Students are only having classes from 8:00 to 12:00 or from 14:00 to 18:00. Students DO NOT have classes at 13:00"
-                            "Lunch time is from 12:00 to 14:00 and there are no classes and dinner time is from 18:00 to 20:00 and there are no classes. " +
-                            "The student must start and end his day at home." + 
-                            "Your response should be in the following JSON format. Do not print any other information before or after the schedule:" +
-                            '{"time": "current time", "location": "current location", "activity": "current activity"}'
+                            "RU, RA and RS are restaurants where students go to have breakfast, luch and dinner. " +
+                            "Every student must have lunch and dinner at some of the three restaurants and they take at most 1 hour eat. " +
+                            "Students don't relax at RU, RA and RS" +
+                            "Students are only having classes from 8 to 12 or from 14 to 18. Students DO NOT have classes at 13"
+                            "Lunch time is from 12 to 14 and there are no classes and dinner time is from 18 to 20 and there are no classes. " +
+                            "The student must start and end his day at home. You must create an entry for every 1 hour. The time format is 'hour'" + 
+                            "Your response should be in a JSON format showing the current location and current activity, always start at time 7. Follow the example: " +
+                            "'7': {'location':'HOME', 'activity':'wake up'}" 
         },
         {
             "role": "user",
             "content": instructions_content,
         }
     ]
-    chat_completion = client.chat.completions.create(messages=message, model="llama3-8b-8192", temperature=0.1)
+    chat_completion = client.chat.completions.create(messages=message, model="llama3-8b-8192", temperature=0.4, response_format={"type": "json_object"})
 
     return chat_completion.choices[0].message.content
 
-student_info = "Chemistry Engineering student. He has most classes at FEQ and has luch at RU and dinner at RA. Today is monday. The student starts and ends his day at home"
-print((getResponse_trip(student_info)))
+# student_info = "Biology student. He has most classes at FCM and IB and has luch at RU and dinner at RS. The student wakes up at 7 and is never late to classes. The student lives close to the university and likes to relax at home whenever possible. The student starts and ends his day at home"
+# print(getResponse_trip(student_info))
