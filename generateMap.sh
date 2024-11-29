f4270325432eefe8f3f58d2f648c1e71cc72be92
RenanFlorencio <renanm.florencio@gmail.com>
2024-11-29 17:56:57 -0300

    Allowing to easily use the code for different maps

---
+++ generateMap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This script DOES NOT END AUTOMATICALLY. Please close SUMO manually after generating the map files and then CTRL+C the terminal."
+
+# Generating map files
+read -p "Enter the name of the output folder: " folder
+
+# Run osmWebWizard.py (this will block the script)
+python3 /usr/share/sumo/tools/osmWebWizard.py -o "$folder"
