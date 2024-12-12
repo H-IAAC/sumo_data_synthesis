1d390b540573c7dd684d246b9829699744fc57de
RenanFlorencio <renanm.florencio@gmail.com>
2024-12-12 16:20:49 -0300

    automatic creating plots based on style

--- scripts/LLAMAconnect.py
+++ scripts/LLAMAconnect.py
@@ -8,48 +8,6 @@ client = Groq(
     api_key = os.getenv("GROQ_API_KEY"),
 )
 
-def getResponse_trip_unicamp(student_info):
-
-    instructions_content = (f"Plan the information of the following student: {student_info}. Students do not have classes during lunch and dinner time" 
-                            )
-    message = [
-        {
-            "role": "system",
-            "content": "You have to plan the routine of multiple students that are going to be between important places inside UNICAMP, you can move them between any of " + 
-                            "the following places: HOME, RU, RA, RS, BC, FEQ, IC, IFGW, IMECC, FEF, IB, IQ, IG, IB, IMECC, IFGW, IC, FEEC. FEE does not exist. " +
-                            "The trip must make sense on the given context. For example, a Chemistry student would spend most of their time. " +
-                            "at IQ, while a Computer Science student would spend most of their time at IC. " +
-                            "Chemistry students spend most of ther time at IQ and IB." +
-                            "Computer Science students spend most of their time at IC." +
-                            "Physics students spend most of their time at IFGW." +
-                            "Math students spend most of their time at IMECC." +
-                            "Physical Education students spend most of their time at FEF." +
-                            "Geography and Geology students spend most of their time at IG." +
-                            "Biology students spend most of their time at IB." +
-                            "Computer Engineering students spend most of their time at FEEC." +
-                            "RU, RA and RS are restaurants where students go to have breakfast, luch and dinner, or they can decide to go home. " +
-                            "Every student must have lunch and dinner at some of the three restaurants and they take at most 1 hour eat. " +
-                            "Students can only have lunch and dinner once per day." +
-                            "Students do not study, spend their free time or relax at RU, RA and RS. " +
-                            "Students pratice sports at FEF. " +
-                            "Students go to BC or to any other institute to study during their free time. " +
-                            "Students are only having classes from 8 to 12 or from 14 to 18. Students DO NOT have classes at 13." +
-                            "A few students do not have all the classes, so they can have free time during the day." +
-                            "All the engineering students have classes all day long." +
-                            "Do not do the same trip for all students, they have different routines." +
-                            "Lunch time is from 12 to 14 and there are no classes and dinner time is from 18 to 20 and there are no classes. " +
-                            "The student must start and end his day at home. You must create an entry for every 1 hour. The time format is 'hour'" + 
-                            "Each student is defined by a trip. Your response should be in a JSON format showing the current location and current activity, always start at time 7. Follow the example: " +
-                            "'trip1': {7': {'location':'HOME', 'activity':'wake up' }, 'trip2': {'8': {'location':'HOME', 'activity':'breakfast' } }" 
-        },
-        {
-            "role": "user",
-            "content": instructions_content,
-        }
-    ]
-    chat_completion = client.chat.completions.create(messages=message, model="llama3-8b-8192", temperature=0.4, response_format={"type": "json_object"})
-
-    return chat_completion.choices[0].message.content
 
 def getResponse(message):
     # Returns a regular LLAMA response
@@ -67,6 +25,7 @@ def getResponse(message):
     return chat_completion.choices[0].message.content
 
 def getResponse_trip(student_info, places):
+    # Generate individual responses for the trips
 
     institute = places['institute']
     university = places['university'][0]
@@ -140,3 +99,26 @@ def generate_response_trips(student_info, places, number_of_trips=5):
                 print(f"Error generating response {e}. Trying a new one.")
 
     return responses
+
+def generate_range_parameters(parameters, styles):
+    # Generates the range of parameters for the vehicle types. Used in vehParameters.py
+
+    instructions_content = (f"Give the range of values for the following styles: {styles}." 
+                            )
+    message = [
+        {
+            "role": "system",
+            "content": "You need to return range of values in JSON for parameters that represent how a driver behaves in traffic, give an explanation for why you picked each value. Following, there is a list of parameter, total possible range and description:\n {parameters}.\nThe more aggressive a driver is, the less they tend to cooperate in traffic and the more selfish they are. Do not go beyond the range restriction for the parameters. The response should be generated in the following format: {{'parameter': {{'style': {{'explanation': 'string', 'min': value, 'max': value}}}}}}. For example, if the styles are aggressive and normal: {{'lcCooperative': {{'aggressive': {{'explanation': 'aggresive drivers are not very cooperative', 'min': 0.2, 'max': 0.5}}, 'normal': {{'explanation': 'normal drivers are cooperative', 'min': 0.5, 'max': 0.8}}}}}}.".format(parameters=parameters)
+        },
+        {
+            "role": "user",
+            "content": instructions_content,
+        }
+    ]
+    try:
+        chat_completion = client.chat.completions.create(messages=message, model="llama3-8b-8192", temperature=0.5, response_format={"type": "json_object"})
+    except Exception as e:
+        print(f"Error generating response {e}. Trying a new one.")
+        return generate_range_parameters(parameters, styles)
+
+    return chat_completion.choices[0].message.content
